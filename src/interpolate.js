6aadc5d83da4e41c13c153a83954b45318bb2562
JonasRossou123 <jonas.rossou@hotmail.com>
2022-03-07 15:17:58 +0100

    should ignore an open delimiter without a closing delimiter OK

--- src/interpolate.js
+++ src/interpolate.js
@@ -2,17 +2,20 @@ const interpolate = (value, session = {}, options = {}) => {
     //TODO
     const regex = new RegExp(`(?<=${options.leftDelimiter})(.*?)(?=${options.rightDelimiter})`, 'g');
     const variablesInString = value.match(regex);
+    console.log(variablesInString)
     let newStr = value;
 
-    variablesInString.forEach(element => {
-        let regexReplacer = new RegExp(`${options.leftDelimiter}\\b${element}\\b${options.rightDelimiter}`, 'g');
-        session.hasOwnProperty(element) ? newStr = newStr.replace(regexReplacer, session[element]) : newStr = newStr.replace(regexReplacer, "");
-    });
+    if (variablesInString != null){
+        variablesInString.forEach(element => {
+            let regexReplacer = new RegExp(`${options.leftDelimiter}\\b${element}\\b${options.rightDelimiter}`, 'g');
+            session.hasOwnProperty(element) ? newStr = newStr.replace(regexReplacer, session[element]) : newStr = newStr.replace(regexReplacer, "");
+        });
+    }
     return newStr;
 };
 
-const result = interpolate('Hi {firstname}, how are you today {lastname}?', {firstname: 'Jonas', lastname: 'Doe'}, {leftDelimiter: '{', rightDelimiter: '}'});
+const result = interpolate('Hi {firstname, how are you today?', {firstname: 'Jonas'}, {leftDelimiter: '{', rightDelimiter: '}'});
 console.log(result)
 
-/*module.exports = { interpolate }*/
+module.exports = { interpolate }
 
